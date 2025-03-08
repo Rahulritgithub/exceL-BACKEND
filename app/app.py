6f6ccb9afbc5c122a81537c5e2f10210840c1338
Rahulritgithub <rahulshanoct7@gmail.com>
2025-03-08 10:03:39 +0530

    chsanged file path 08-03-25

--- app/app.py
+++ app/app.py
@@ -9,7 +9,7 @@ import pandas as pd
 import re
 import io
 from io import BytesIO
-from app.parser_script import parse_log_file, parse_log_file2, parse_log_file3
+from parser_script import parse_log_file, parse_log_file2, parse_log_file3
 import matplotlib.pyplot as plt
 import base64
 import gspread
